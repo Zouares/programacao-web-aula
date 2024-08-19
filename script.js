1416016ce56f0e7c2ba3349f18c13efef62e60f6
Zouares <gabrielssoaress02@gmail.com>
2024-08-19 09:17:44 -0300

    adição da caixa do EMAIL

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ function submeter() {
     
     let elementoNome = document.getElementById ("nome-input").value; 
     let elementoIdade = document.getElementById ("idade-input").value;
+    let elementoEmail = document.getElementById ("email-input").value;
     let elementoCpf = document.getElementById ("cpf-input").value;
    console.log(elementoNome);
    console.log(elementoIdade);
    console.log(elementoCpf);
+   console.log(elementoEmail);
+
   
+   console.log(valideEmail(elementoEmail));
+}
+
+function valideEmail(email) {
+    return /^[/w]+@[/w]+.[a-zA-Z]/.test(email)
+
+
+    
 }
